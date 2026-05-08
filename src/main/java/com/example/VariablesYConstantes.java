47b5d26f89ee828e9dd67a7b61c522692a45a18d
cxcristian <cristiandavid3d@gmail.com>
2026-05-08 11:22:10 -0500

    Inicio de actividad

--- src/main/java/com/example/VariablesYConstantes.java
+++ src/main/java/com/example/VariablesYConstantes.java
@@ -3,13 +3,29 @@ package com.example;
 public class VariablesYConstantes {
     public static void demostrarDeclaracionAsignacion() {        
         // TODO
+        int edad = 18;
+        double estatura = 1.75;
+        System.out.println(edad); 
+        System.out.println(estatura); 
+
     }
 
     public static void demostrarConvencionesNombres() {
         // TODO
+         int edadPermitida = 20;
+         int edadNoPermitida = -15;
+         System.out.println("La edad permitida es:" + edadPermitida);
+         System.out.println("La edad no permitida es:" + edadNoPermitida);
     }
 
     public static void demostrarConstantes() {
         // TODO
+        final double PI = 3.14159265358979323846;
+        
+        double PI = 3.1899;
+
+        // Este es el error que aparece a la hora de asignarle otro valor:
+        //Exception in thread "main" java.lang.Error: Unresolved compilation problem: Duplicate local variable PI
+  
     }
 }
