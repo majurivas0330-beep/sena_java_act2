91f4b12783edb2ea8d3dcc67a0174c46fc8ae5cc
cxcristian <cristiandavid3d@gmail.com>
2026-05-29 10:29:13 -0500

    Avance de ejercicios Java

--- src/main/java/com/example/TiposDeDatosPrimitivos.java
+++ src/main/java/com/example/TiposDeDatosPrimitivos.java
@@ -2,22 +2,55 @@ package com.example;
 
 public class TiposDeDatosPrimitivos {
     public static void demostrarEnteros() {
-        // TODO
+
+        byte numeroByte = 127;
+        short numeroShort = 32000;
+        int numeroInt = 2000000;
+        long numeroLong = 999999999L;
+
+        System.out.println("=== ENTEROS ===");
+        System.out.println("byte: " + numeroByte);
+        System.out.println("short: " + numeroShort);
+        System.out.println("int: " + numeroInt);
+        System.out.println("long: " + numeroLong);
     }
 
     public static void demostrarFlotantes() {
-        // TODO
+
+        float precioCafe = 4500.50f;
+        double precioCafePremium = 8500.99;
+
+        System.out.println("\n=== FLOTANTES ===");
+        System.out.println("Precio café (float): " + precioCafe);
+        System.out.println("Precio café premium (double): " + precioCafePremium);
     }
 
     public static void demostrarCaracteres() {
-        // TODO
+
+        char inicial = 'M';
+
+        System.out.println("\n=== CARACTERES ===");
+        System.out.println("Inicial: " + inicial);
     }
 
     public static void demostrarBooleanos() {
-        // TODO
+
+        boolean luzEncendida = true;
+
+        System.out.println("\n=== BOOLEANOS ===");
+        System.out.println("Luz encendida: " + luzEncendida);
     }
 
     public static void demostrarValoresPorDefecto() {
-        // TODO
+
+        System.out.println("\n=== VALORES POR DEFECTO ===");
+
+        int numero = 0;
+        boolean estado = false;
+        char letra = 'A';
+
+        System.out.println("int: " + numero);
+        System.out.println("boolean: " + estado);
+        System.out.println("char: " + letra);
     }
 }
--- src/main/java/com/example/VariablesYConstantes.java
+++ src/main/java/com/example/VariablesYConstantes.java
@@ -14,15 +14,19 @@ public class VariablesYConstantes {
         // TODO
          int edadPermitida = 20;
          int edadNoPermitida = -15;
-         System.out.println("La edad permitida es:" + edadPermitida);
-         System.out.println("La edad no permitida es:" + edadNoPermitida);
+
+         System.out.println("La edad permitida es" + edadPermitida);
+         System.out.println("La edad no permitida es" + edadNoPermitida);
+      
     }
 
     public static void demostrarConstantes() {
         // TODO
         final double PI = 3.14159265358979323846;
         
-        double PI = 3.1899;
+        PI = 3.1899;
+
+        System.out.println("El valor de PI es:" + PI);
 
         // Este es el error que aparece a la hora de asignarle otro valor:
         //Exception in thread "main" java.lang.Error: Unresolved compilation problem: Duplicate local variable PI
